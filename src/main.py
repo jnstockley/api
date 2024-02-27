08bc8e80ee1777716ecb6850c53faa37b2782e4b
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2024-02-26 19:24:19 -0600

    Fixed linting issues

--- src/main.py
+++ src/main.py
@@ -3,19 +3,16 @@ from fastapi import FastAPI
 
 from controllers import docker, health_check
 
-version = toml.load("pyproject.toml")['tool']['poetry']['version']
+version = toml.load("pyproject.toml")["tool"]["poetry"]["version"]
 
-app = FastAPI(docs_url='/docs/swagger',
-              redoc_url='/docs/redoc',
-              title='Jnstockley API',
-              version=version,
-              contact={
-                  "name": 'Jack Stockley'
-              },
-              license_info={
-                  'name': 'MIT License',
-                  'url': 'https://mit-license.org'
-              })
+app = FastAPI(
+    docs_url="/docs/swagger",
+    redoc_url="/docs/redoc",
+    title="Jnstockley API",
+    version=version,
+    contact={"name": "Jack Stockley"},
+    license_info={"name": "MIT License", "url": "https://mit-license.org"},
+)
 
 app.include_router(health_check.router)
 app.include_router(docker.router)

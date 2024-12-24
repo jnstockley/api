3b053b9196cc9414db09d42bbd277a5a658c6e7f
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2024-12-23 19:51:56 -0600

    Fix health check

--- src/controllers/health_check.py
+++ src/controllers/health_check.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from sqlalchemy import text
 
 from database import db_dependency
 
@@ -9,7 +10,9 @@ router = APIRouter(prefix="/health-check")
 async def health_check(db: db_dependency):
     try:
         # Execute a simple query to check the database connection
-        db.execute("SELECT 1")
+        db.execute(text('SELECT 1'))
         return {"status": "ok"}
-    except Exception:
+    except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail="Database connection failed")
+

3aa0e2088bab9b43f97773b0fe3e95c068ae6ddd
Jack Stockley <jack@jstockley.com>
2026-08-01 10:39:20 -0500

    Fix MR conflicts

--- src/util/healthcheck.py
+++ src/util/healthcheck.py
@@ -1,16 +1,12 @@
 import sys
 
-<<<<<<< HEAD
 from sqlalchemy import select
 
 from database import get_db
-=======
->>>>>>> external/main
 from util.logging import logger
 
 
 def healthcheck() -> bool:
-<<<<<<< HEAD
     try:
         # get_db() is a generator, so we need to get the session from it
         db = next(get_db())
@@ -20,7 +16,3 @@ def healthcheck() -> bool:
     except Exception as e:  # noqa: BLE001
         logger.error(e)
         sys.exit(1)
-=======
-    logger.info("Healthcheck passed!")
-    sys.exit(0)
->>>>>>> external/main
